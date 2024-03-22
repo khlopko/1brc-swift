3073a3fad8a08f6d9d34ca29e3678a583e8c90f3
kyrylokhlopko <khlopko@gmail.com>
2024-03-22 17:43:03 +0100

    Use FNV-1a hashing algorithm (thought previous one gave collisions); Refactored a bit and cleaned project

--- Sources/1brc-swift/app.swift
+++ Sources/1brc-swift/app.swift
@@ -1,72 +1,18 @@
-// The Swift Programming Language
-// https://docs.swift.org/swift-book
+// 1brc implementation in Swift
+// (c) Kyrylo Khlopko
 
 import Foundation
 
-let path = "../1brc/measurements.txt"
-
 @main
 struct Main {
-
-    static func main() async throws {
-        let results = await withTaskGroup(of: [Int: Measurement].self) { group in
-            let totalChunks: UInt64 = 2048
-            let file = fopen(path, "r")!
-            defer {
-                fclose(file)
-            }
-            fseek(file, 0, SEEK_END)
-            let max = ftell(file)
-            let chunkSize = Int(ceil(Double(max) / Double(totalChunks)))
-            rewind(file)
-            var offset: Int = 0
-            var chunkStart: Int = 0
-            var chunkN = 0
-            var c = fgetc(file)
-            while feof(file) == 0 {
-                chunkStart = offset
-                offset += chunkSize
-                fseek(file, offset, SEEK_SET)
-                while feof(file) == 0 && c != 10 {
-                    c = fgetc(file)
-                }
-                offset = ftell(file)
-                if offset > max {
-                    offset = max
-                }
-                let size = offset - chunkStart
-                group.addTask { [chunkN, chunkStart] in
-                    let reader = ReadByChunks()
-                    let data = await reader.run(chunkN, offset: chunkStart, chunkSize: size)
-                    let parser = ParseLines()
-                    let partialResult = await parser.run(data: data)
-                    return partialResult
-                }
-                c = fgetc(file)
-                chunkN += 1
-            }
-            var partialResults: [Int: Measurement] = Dictionary(minimumCapacity: 500)
-            for await result in group {
-                for (key, measurement) in result {
-                    if let existing = partialResults[key] {
-                        partialResults[key] = Measurement(
-                            name: existing.name,
-                            min: min(existing.min, measurement.min),
-                            avg: existing.avg + measurement.avg,
-                            max: Swift.max(existing.max, measurement.max),
-                            count: existing.count + measurement.count
-                        )
-                    } else {
-                        partialResults[key] = measurement
-                    }
-                }
-            }
-            return partialResults
-        }
-        Measurement.display(results: results)
+    static func main() async {
+        let path = "../1brc/measurements.txt"
+        let app = App(path: path)
+        await app.run()
     }
-
 }
+
+typealias PartialResult = [UInt64: Measurement]
 
 struct Measurement {
     let name: ArraySlice<UInt8>
@@ -76,34 +22,82 @@ struct Measurement {
     let count: Int
 }
 
-extension Measurement {
+struct App {
+    let path: String
+
     @inline(__always)
-    static func display(results: borrowing [Int: Measurement]) {
-        print("{", terminator: "")
-        let prepared: [(String, Measurement)] = results.map { (String(cString: Array($1.name) + [0]), $1) }
-        let out: String = prepared
-            .sorted { lhs, rhs in
-                lhs.0 < rhs.0
+    func run() async {
+        let results = await withTaskGroup(of: PartialResult.self) { group in
+            await process(inside: &group)
+        }
+        let printer = Printer()
+        printer.run(on: results.values)
+    }
+
+    @inline(__always)
+    private func process(inside group: inout TaskGroup<PartialResult>) async -> PartialResult {
+        let totalChunks: UInt64 = 2048
+        let file = fopen(path, "r")!
+        defer {
+            fclose(file)
+        }
+        fseek(file, 0, SEEK_END)
+        let max = ftell(file)
+        let chunkSize = Int(ceil(Double(max) / Double(totalChunks)))
+        rewind(file)
+        var offset: Int = 0
+        var chunkStart: Int = 0
+        var c = fgetc(file)
+        while feof(file) == 0 {
+            chunkStart = offset
+            offset += chunkSize
+            fseek(file, offset, SEEK_SET)
+            while feof(file) == 0 && c != 10 {
+                c = fgetc(file)
             }
-            .map { (name, value) -> String in
-                String(
-                    format: "%@=%.1f/%.1f/%.1f", 
-                    name, 
-                    Double(value.min) / 10.0,
-                    Double(value.avg) / 10.0 / Double(value.count),
-                    Double(value.max) / 10.0
-                )
+            offset = ftell(file)
+            if offset > max {
+                offset = max
             }
-            .joined(separator: ", ")
-        print(out, terminator: "")
-        print("}")
+            let size = offset - chunkStart
+            group.addTask { [chunkStart] in
+                let reader = ReadByChunks(path: path)
+                let data = await reader.run(offset: chunkStart, chunkSize: size)
+                let parser = ParseLines()
+                let partialResult = await parser.run(data: data)
+                return partialResult
+            }
+            c = fgetc(file)
+        }
+        var partialResults = PartialResult(minimumCapacity: 500)
+        for await result in group {
+            for (key, measurement) in result {
+                if let existing = partialResults[key] {
+                    partialResults[key] = Measurement(
+                        name: existing.name,
+                        min: min(existing.min, measurement.min),
+                        avg: existing.avg + measurement.avg,
+                        max: Swift.max(existing.max, measurement.max),
+                        count: existing.count + measurement.count
+                    )
+                } else {
+                    partialResults[key] = measurement
+                }
+            }
+        }
+        return partialResults
     }
 }
 
 actor ReadByChunks {
+    private let path: String
+
+    init(path: String) {
+        self.path = path
+    }
 
     @inline(__always)
-    func run(_ chi: Int, offset: Int, chunkSize: Int) -> [UInt8] {
+    func run(offset: Int, chunkSize: Int) -> [UInt8] {
         let file: UnsafeMutablePointer<FILE> = fopen(path, "r")!
         fseek(file, offset, SEEK_SET)
         let buffer = UnsafeMutableRawPointer.allocate(byteCount: chunkSize, alignment: MemoryLayout<UInt8>.alignment)
@@ -116,24 +110,26 @@ actor ReadByChunks {
     }
 }
 
+private let fnv1aBasis: UInt64 = 14695981039346656037
+private let fnv1aPrime: UInt64 = 1099511628211
+
 actor ParseLines {
     @inline(__always)
-    func run(data: consuming [UInt8]) -> [Int: Measurement] {
-        var results: [Int: Measurement] = Dictionary(minimumCapacity: 500)
+    func run(data: consuming [UInt8]) -> PartialResult {
+        var results = PartialResult(minimumCapacity: 500)
         var i = 0
         var temp: Int = 0
         var sign: Int = 1
         var k = i
         var name: ArraySlice<UInt8> = []
-        let mask: Int = 0x7FFFFFFF
         while i < data.count {
             k = i
-            var hash: Int = 0
+            var hash = fnv1aBasis
             while data[i] != 59 {
-                hash = 31 &* hash &+ Int(data[i])
+                hash ^= UInt64(data[i])
+                hash = hash.multipliedReportingOverflow(by: fnv1aPrime).partialValue
                 i += 1
             }
-            hash &= mask
             name = data[k..<i]
             i += 1 // skip the semicolon
             temp = 0
@@ -150,20 +146,46 @@ actor ParseLines {
             temp = temp * 10 + Int(data[i]) - 48
             temp *= sign
             i += 1 // skip the newline
-            if let measurement = results[hash] {
+            switch results[hash] {
+            case let .some(measurement):
                 results[hash] = Measurement(
-                    name: name,
+                    name: measurement.name,
                     min: min(measurement.min, temp),
                     avg: measurement.avg + temp,
                     max: Swift.max(measurement.max, temp),
                     count: measurement.count + 1
                 )
-            } else {
+            case .none:
                 results[hash] = Measurement(name: name, min: temp, avg: temp, max: temp, count: 1)
             }
             i += 1
         }
         return results
+    }
+}
+
+/// Prints the final results to stdout.
+struct Printer {
+    @inline(__always)
+    func run(on results: any Collection<Measurement>) {
+        print("{", terminator: "")
+        let prepared: [(String, Measurement)] = results.map { (String(cString: Array($0.name) + [0]), $0) }
+        let out: String = prepared
+            .sorted { lhs, rhs in
+                lhs.0 < rhs.0
+            }
+            .map { (name, value) -> String in
+                String(
+                    format: "%@=%.1f/%.1f/%.1f", 
+                    name, 
+                    Double(value.min) / 10.0,
+                    Double(value.avg) / 10.0 / Double(value.count),
+                    Double(value.max) / 10.0
+                )
+            }
+            .joined(separator: ", ")
+        print(out, terminator: "")
+        print("}")
     }
 }
 
